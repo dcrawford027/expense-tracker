f4e7eda84274de22f8bbcfc12790d62decfc4efc
Dustin Crawford <dustin.crawford.2@gmail.com>
2022-09-19 17:46:10 -0400

    separated stateful and stateless components

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,22 +1,20 @@
 import { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
+import ExpensesFilter from './ExpensesFilter';
 import './ExpenseList.css';
 
 const ExpenseList = props => {
     const { expenses } = props;
-    const [selectedFilter, setSelectedFilter] = useState('');
+    const [selectedFilter, setSelectedFilter] = useState('2020');
 
     const filterChangeHandler = filterValue => {
       setSelectedFilter(filterValue);
-      console.log('From ExpenseList');
-      console.log(filterValue);
     }
 
     return (
       <Card className='expenses'>
-        <ExpensesFilter onFilterChange={filterChangeHandler} />
+        <ExpensesFilter onFilterChange={filterChangeHandler} filterYear={selectedFilter}/>
         <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
         <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
         <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
