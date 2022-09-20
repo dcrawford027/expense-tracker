3ce3bcd1b17931dea6e51c302be1a113a802fdd4
Dustin Crawford <dustin.crawford.2@gmail.com>
2022-09-20 14:49:15 -0400

    Rendered data list with filter

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,26 +1,37 @@
-import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import ExpensesFilter from './ExpensesFilter';
-import './ExpenseList.css';
+import { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./ExpenseList.css";
 
-const ExpenseList = props => {
-    const { expenses } = props;
-    const [selectedFilter, setSelectedFilter] = useState('2020');
+const ExpenseList = (props) => {
+  const { expenses } = props;
+  const [selectedFilter, setSelectedFilter] = useState("2020");
 
-    const filterChangeHandler = filterValue => {
-      setSelectedFilter(filterValue);
-    }
+  const filterChangeHandler = (filterValue) => {
+    setSelectedFilter(filterValue);
+  };
 
-    return (
-      <Card className='expenses'>
-        <ExpensesFilter onFilterChange={filterChangeHandler} filterYear={selectedFilter}/>
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} />
-      </Card>
-    );
-}
+  const filteredExpenses = expenses.filter(
+    expense => expense.date.getFullYear().toString() === selectedFilter
+  );
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        onFilterChange={filterChangeHandler}
+        filterYear={selectedFilter}
+      />
+      {filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+};
 
 export default ExpenseList;
